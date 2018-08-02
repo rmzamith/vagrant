5ea8dd6dd0996c5d56abef7c36f5b35f36ccddec
Raphael Zamith <rmzamith@gmail.com>
2018-08-02 02:44:35 -0300

    fixes on bootstrap script for ubuntu docker image

--- ubuntu_docker/scripts/bootstrap.sh
+++ ubuntu_docker/scripts/bootstrap.sh
@@ -5,16 +5,28 @@ apt-get update
 apt-get upgrade
 
 #install git
-apt-get install git
+apt-get -y install git
+
+#install tree
+apt-get -y install tree
 
 # install docker client
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 DOCKER_REPO_FILE_PATH="/etc/apt/sources.list.d/docker.list"
 touch $DOCKER_REPO_FILE_PATH
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > $DOCKER_REPO_FILE_PATH
 apt-get update
 apt-get purge lxc-docker* docker.io*
-apt-get install docker-engine
+apt-get -y install docker-engine
 
 # configure docker server
-chkconfig docker on
-service docker start
+update-rc.d docker defaults
+service docker restart
+
+# create workspace folder
+cd /home/vagrant/
+mkdir workspace
+cd workspace
+
+# clone a docker sample application to workspace
+git clone https://github.com/spkane/docker-node-hello.git
