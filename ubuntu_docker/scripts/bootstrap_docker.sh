6f93a2876bd71813a7affa2e81b9a50e9f4ac1c7
Raphael Zamith <raphael@epicom.com.br>
2018-08-02 15:57:50 -0300

    trying to initialize docker swarm cluster from vagrantfile

--- ubuntu_docker/scripts/bootstrap_docker.sh
+++ ubuntu_docker/scripts/bootstrap_docker.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
-
-# update packages
-apt-get update
-apt-get upgrade
-
-#install git
-apt-get -y install git
-
-#install tree
-apt-get -y install tree
-
-# install virtual box
-apt-get -y install virtualbox 
-
 # install docker client
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 DOCKER_REPO_FILE_PATH="/etc/apt/sources.list.d/docker.list"
@@ -31,13 +16,6 @@ base=https://github.com/docker/machine/releases/download/v0.14.0
 curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine
 sudo install /tmp/docker-machine /usr/local/bin/docker-machine
 
-# create workspace folder
-cd /home/vagrant/
-mkdir workspace
-cd workspace
-
-# clone a docker sample application to workspace
-git clone https://github.com/spkane/docker-node-hello.git
 
 # create docker swarm VMs on VirtualBox
 docker-machine create --driver virtualbox myvm1
@@ -45,3 +23,8 @@ docker-machine create --driver virtualbox myvm2
 docker-machine create --driver virtualbox myvm3
 docker-machine create --driver virtualbox myvm4
 docker-machine create --driver virtualbox myvm5
+docker-machine create --driver virtualbox myvmSwarmManager
+
+# initialize myvmSwarmManager as a swarm manager
+sm_ip=docker-machine ip myvmSwarmManager
+docker-machine ssh myvm1 "docker swarm init --advertise-addr $sm_ip"
