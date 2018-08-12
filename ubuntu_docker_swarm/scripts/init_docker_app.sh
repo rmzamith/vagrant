abfabdf613872de4dc5de6ef0f9599c443d943c2
Raphael Zamith <raphael@epicom.com.br>
2018-08-12 03:01:17 -0300

    finishing part 4 of tutorial docker swarm

--- ubuntu_docker_swarm/scripts/init_docker_app.sh
+++ ubuntu_docker_swarm/scripts/init_docker_app.sh
@@ -2,7 +2,13 @@
 cd /home/vagrant/workspace
 
 #build image
-docker build -t friendlyhello .
+#docker build -t friendlyhello .
 
 #run image
-docker run -d -p 4000:80 friendlyhello
+#docker run -d -p 4000:80 friendlyhello
+
+#init swarm
+docker swarm init
+
+#init service
+docker stack deploy -c docker-compose.yml getstartedlab
